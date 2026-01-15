839c8d621bdf6803f1a2000f6210d4874cfc30e7
ZhichGaming <nick.zhicheng@gmail.com>
2026-01-15 10:05:19 -0500

    chore: root website base path

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/mrt-website' : '',
+  basePath: '',
   trailingSlash: true,
 };
 
